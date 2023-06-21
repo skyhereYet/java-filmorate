f6c95579b3c54581f9b4e135fa8cddb858a8152a
skyhereYet <mr.mvpopov@yandex.ru>
2023-06-22 00:35:05 +0300

    feat: add services, friends, likes

--- src/main/java/spring/web/utils/FilmReleaseDateValidator.java
+++ src/main/java/spring/web/utils/FilmReleaseDateValidator.java
@@ -4,7 +4,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
-public class FilmReleaseDateValidator implements ConstraintValidator<ReleaseDateChecker, LocalDate>{
+public class FilmReleaseDateValidator implements ConstraintValidator<ReleaseDateChecker, LocalDate> {
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
         return date.isAfter(LocalDate.of(1895, 12, 27));
